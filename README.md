4010ce28e2119d95b1fdec82c1bfe1f51dc1f792
Chris Leon <christopher.leon@gmail.com>
2022-07-12 06:51:44 -0500

    Created base app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Overview
+This is the repo containing a sample app used during [LaunchDarkly](https://launchdarkly.com) Workshop sessions.
+
+See [here](../README.md) for more info.
---
+++ workshop/step1/src/index.js
@@ -0,0 +1,19 @@
+import { createRoot } from 'react-dom/client';
+import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk';
+import App from './App';
+import './index.css';
+
+(async () => {
+  const rootElement = document.getElementById('root');
+  const root = createRoot(rootElement);
+
+  const LDProvider = await asyncWithLDProvider({
+    user: { key: 'user@workshop.org' },
+    clientSideID: process.env.REACT_APP_LD_CLIENT_SIDE_ID
+  });
+
+  root.render(
+    <LDProvider>
+      <App />
+    </LDProvider>);
+})();
